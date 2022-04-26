67fddcb02910ace9adb1161e8610c4dc330b078c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-26 09:40:08 +0530

    updated

---
+++ src/main/java/mainpage/testNG.java
@@ -0,0 +1,10 @@
+package mainpage;
+
+import org.testng.annotations.Test;
+
+public class testNG {
+    @Test
+    public void testcase(){
+        System.out.println("This is the test case");
+    }
+}
