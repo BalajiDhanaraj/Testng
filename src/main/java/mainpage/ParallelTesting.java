fd6d748057648ffa02db9b8485563d02eb33c884
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-27 10:58:46 +0530

    updated

---
+++ src/main/java/mainpage/ParallelTesting.java
@@ -0,0 +1,26 @@
+package mainpage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ParallelTesting {
+
+    @Test
+    public void openGoogle(){
+        System.setProperty("webriver.chrome.driver","/Volumes/Macintosh HD/For Mac/java text and soft file and java IDE/Intellij java project/Drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.quit();
+    }
+    @Test
+    public void openBing(){
+        System.setProperty("webriver.chrome.driver","/Volumes/Macintosh HD/For Mac/java text and soft file and java IDE/Intellij java project/Drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.bing.com");
+        driver.quit();
+    }
+
+
+
+}
