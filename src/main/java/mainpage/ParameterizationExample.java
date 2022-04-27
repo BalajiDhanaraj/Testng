e954256c811faade8b0e9384330c99fcc1f35518
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-27 10:43:00 +0530

    updated

--- src/main/java/mainpage/ParameterizationExample.java
+++ src/main/java/mainpage/ParameterizationExample.java
@@ -1,5 +1,15 @@
 package mainpage;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class ParameterizationExample {
+
+    // the parameterization is the data given in runtime of the testcase
+    @Test
+    @Parameters({"Name", "Age"})
+    public void printName(String name,String age){
+        System.out.println("Name "+name+" "+age);
+    }
 
 }
