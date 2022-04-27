20467513e02f288168555c055adab70a696e3953
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-27 09:47:28 +0530

    updated

--- src/main/java/mainpage/GroupingExample.java
+++ src/main/java/mainpage/GroupingExample.java
@@ -1,4 +1,33 @@
 package mainpage;
 
+import org.testng.annotations.Test;
+
 public class GroupingExample {
+
+    // the groups = {"name of the group"} --> this tag is used to group the methods in single file
+    @Test(groups = {"Apple"})
+    public void Apple1(){
+        System.out.println("apple");
+    }
+    @Test(groups = {"Apple"})
+    public void Apple2(){
+        System.out.println("apple");
+    }
+    @Test(groups = {"Blackberry"})
+    public void Blackberry1(){
+        System.out.println("Black berry");
+    }
+    @Test(groups = {"Blackberry"})
+    public void Blackberry2(){
+        System.out.println("Black berry");
+    }
+    @Test(groups = {"Htc"})
+    public void htc1(){
+        System.out.println("Htc");
+    }
+    @Test(groups = {"Htc"})
+    public void htc2(){
+        System.out.println("Htc");
+    }
+
 }
